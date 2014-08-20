796015772955ea4d5bcb289103465a30c697d1e0
sahaaron <aaronmyin@gmail.com>
2014-08-19 23:57:17 -0500

    asdasd

--- FineLineIOSApp/company_website/urls.py
+++ FineLineIOSApp/company_website/urls.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.views.generic import View
 from django.http import HttpResponse
-from django.core import serializersfrom FineLineIOSApp.company_website import views
+from django.core import serializers
+from FineLineIOSApp.company_website import views
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
--- FineLineIOSApp/company_website/views.py
+++ FineLineIOSApp/company_website/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.core import serializers
+from FineLineIOSApp.company_website import views
+from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse
+from django.views.generic import View
+from django.http import HttpResponse
 
 class bouncingBots(View):
     def get(self, request,*args,**kwargs):
-        render(request, "bouncingBots.html")
+        return render(request, "bouncingBots.html")
  
