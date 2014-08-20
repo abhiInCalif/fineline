21a279a1e7a749bf03a913f24e05b6f7c249de2d
sahaaron <aaronmyin@gmail.com>
2014-08-20 00:00:09 -0500

    midnight-changes

--- FineLineIOSApp/company_website/urls.py
+++ FineLineIOSApp/company_website/urls.py
@@ -1,9 +1,3 @@
-from django.conf.urls import patterns, include, url
-from django.shortcuts import render
-from django.core.urlresolvers import reverse
-from django.views.generic import View
-from django.http import HttpResponse
-from django.core import serializers
 from FineLineIOSApp.company_website import views
 from django.conf.urls import patterns, include, url
 
--- FineLineIOSApp/company_website/views.py
+++ FineLineIOSApp/company_website/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 from django.core import serializers
-from FineLineIOSApp.company_website import views
-from django.conf.urls import patterns, include, url
-from django.core.urlresolvers import reverse
 from django.views.generic import View
 from django.http import HttpResponse
 
