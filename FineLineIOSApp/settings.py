66bdaebd228915957c6e58bc0bb774bdcaaa19b5
Abhinav Khanna <akhanna@princeton.edu>
2014-08-19 20:59:47 -0700

    updated settings to include new applications

--- FineLineIOSApp/settings.py
+++ FineLineIOSApp/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'FineLineIOSApp.partyorders',
+    'FineLineIOSApp.payment',
+    'FineLineIOSApp.company_website',
     'south',
 )
 
