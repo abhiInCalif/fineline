f054d9752e63cec44cee5c12c1338c75f787ac56
Abhinav Khanna <akhanna@princeton.edu>
2014-09-03 14:25:19 -0700

    updated the admin.py file to register the new models

--- FineLineIOSApp/partyorders/admin.py
+++ FineLineIOSApp/partyorders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from FineLineIOSApp.partyorders.models import ShoppingItem, Delivery, Order, OrderItem
+
+admin.site.register(ShoppingItem)
+admin.site.register(Delivery)
+admin.site.register(Order)
+admin.site.register(OrderItem)
